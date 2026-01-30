2565ff15cd42a7c81ef8c2c4348206e2f79b72e7
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-01-30 05:34:38 -0600

    add default package to flake outputs

--- flake.nix
+++ flake.nix
@@ -3,16 +3,25 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ 
-					  go
-					];
-        };
-      });
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+        ];
+      };
+
+      packages.default = pkgs.buildGoModule {
+        name = "kubefetch";
+        version = "0.9.0";
+        src = ./.;
+        vendorHash = "sha256-qsncOsCxepySJI+rJnzbIGxSWlxMzqShtzcEoJD2UPw=";
+      };
+    });
 }
